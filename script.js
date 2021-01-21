d2e314973e4dc72778054dcd1663f8bb82971244
unknown <>
2021-01-21 17:37:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//get the url
+let url = window.location.search.substring(1);
+
+//split it by ? or &
+let array = url.split(/\?|&/);
+//save items in localStorage
+localStorage.setItem('array', array);
+
+
+//push each element on a new line
+let newArray = [];
+for (i = 0; i < array.length; i++) {
+  newArray.push(array[i] + '<br>');
+}
+
+
+//attach the results to a div element
+document.getElementById("split_url").innerHTML = newArray;
